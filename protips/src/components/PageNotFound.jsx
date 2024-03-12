7bfa6e774b696fb7648ff03fde8cb1e27d2a8150
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-12 10:34:01 +0530

    done

---
+++ protips/src/components/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <center>
+        <img style={{ width: "80%", height: "85vh" }} src="./src/assets/404.gif" alt="" />
+      </center>
+    </div>
+  );
+};
+
+export default PageNotFound;
